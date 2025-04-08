3bcf83ea6dc729ea3c3b7bfa07a05ee0eba962db
Chirag Singh <chiraaajsingh7600@gmail.com>
2025-04-08 12:12:27 +0530

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ io.setMaxListeners(200);
 
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
